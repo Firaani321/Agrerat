49149603836b8cbdcb83f8f7471f30145e37556c
Firaani321 <botserver445566@gmail.com>
2025-08-07 10:24:08 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
-// pages/_app.js (Sangat Sederhana)
+// File: pages/_app.js (Versi BARU yang Lebih Bersih)
+
 import '../styles/globals.css';
-import Layout from '../components/Layout';
+import Link from 'next/link';
+
+function TopBar() {
+  return (
+    <header className="bg-white shadow-sm p-4 sticky top-0 z-10">
+      <div className="max-w-7xl mx-auto">
+        <Link href="/" className="font-bold text-lg text-slate-800 hover:text-blue-600">
+          Dashboard Kasir
+        </Link>
+      </div>
+    </header>
+  );
+}
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <>
+      <TopBar />
       <Component {...pageProps} />
-    </Layout>
+    </>
   );
 }
 
